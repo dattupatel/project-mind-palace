86b42aa09025cb546aa4afcfc4f49e278b0dd097
Alucard Patel <patel.alucard@gmail.com>
2021-07-07 19:20:04 -0400

    fixed

--- server/app.js
+++ server/app.js
@@ -1,31 +1,35 @@
-// const express = require('express');
-
-// const app = express();
-
-// app.use(routes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT);
+const express = require('express');
 
 
+const app = express();
 
-const http = require('http');
-const getFilesAndFolders = require('./utils').getFilesAndFolders;
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+  }));
 
 const PORT = process.env.PORT || 5000;
-const SERVER = http.createServer((req, res) => {
-    const result = getFilesAndFolders('F:/OneDrive/My Documents');
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(result));
-    res.end();
-});
+app.listen(PORT);
 
-SERVER.listen(PORT);
 
-SERVER.on('listening', () => {
-    console.log('[Server]::LISTEN:%s', PORT);
-});
 
-SERVER.on('error', (error) => {
-    throw new Error(`[Server]::ERROR:${error.message}`);
-});
+// const http = require('http');
+// const getFilesAndFolders = require('./utils').getFilesAndFolders;
+
+// const PORT = process.env.PORT || 5000;
+// const SERVER = http.createServer((req, res) => {
+//     const result = getFilesAndFolders('F:/OneDrive/My Documents');
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.write(JSON.stringify(result));
+//     res.end();
+// });
+
+// SERVER.listen(PORT);
+
+// SERVER.on('listening', () => {
+//     console.log('[Server]::LISTEN:%s', PORT);
+// });
+
+// SERVER.on('error', (error) => {
+//     throw new Error(`[Server]::ERROR:${error.message}`);
+// });
